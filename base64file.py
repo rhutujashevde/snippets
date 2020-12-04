9aa843090500fb44062e07ae64cbf100e0c038f1
Rhutuja <44125194+rhutujashevde@users.noreply.github.com>
2020-12-04 12:45:58 +0530

    Update base64file.py

--- base64file.py
+++ base64file.py
@@ -1,7 +1,7 @@
 from urllib2 import urlopen
 import base64
 
-bank_statement_url = "https://lendenclub.s3-ap-southeast-1.amazonaws.com/media/documents/2019/08/10/C22E746984F4C1FB6C2BC229.pdf"
+bank_statement_url = "url"
 file_object = urlopen(bank_statement_url)
 base64_file = base64.b64encode(file_object.read())
 
